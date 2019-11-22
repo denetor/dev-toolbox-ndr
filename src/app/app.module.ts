5654c9e2b7d7fc9ce6cae0d3ff72bccc833809c1
denetor <denetor@gmail.com>
2019-11-22 12:09:35 +0100

    multiconverter, empty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NdrNavbarComponent } from './ndr-navbar/ndr-navbar.component';
+import { NdrMulticonverterComponent } from './ndr-multiconverter/ndr-multiconverter.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NdrNavbarComponent ],
+  declarations: [ AppComponent, HelloComponent, NdrNavbarComponent, NdrMulticonverterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
